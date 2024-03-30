5fc398242eac81888f4ec2c8642a03a90e3cc72f
Juan Huguet <jhuguetospina@gmail.com>
2024-03-30 15:58:38 -0500

    REFACTOR: format and value

--- src/app/request/categories.tsx
+++ src/app/request/categories.tsx
@@ -1,29 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Categories = () => {
+const Categories = ({ setCategory }: { setCategory: any }) => {
   const [categories, setCategories] = useState([]);
-  const category_default = "Categoría"
+  const category_default = "Categoría";
+
   useEffect(() => {
     const loadCategoryFetch = async () => {
-        const response_category = await fetch("http://localhost:5000/category/", {
-          method: "GET",
-        }).then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          return [];
-        });
-        setCategories(response_category);
-      };
+      const response_category = await fetch("http://localhost:5000/category/", {
+        method: "GET",
+      }).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return [];
+      });
+      setCategories(response_category);
+    };
 
     loadCategoryFetch();
   }, []);
 
   return (
-    <select name="category" id="categoryList">
+    <select
+      name="category"
+      id="categoryList"
+      onChange={({ target: { value } }) => {
+        setCategory(value);
+      }}
+    >
       <option defaultValue={category_default}>{category_default}</option>
-      {categories.map(item => (
-        <option key={item["category_id"]} value={item["category_id"]}>{item["name"]}</option>
+      {categories.map((item) => (
+        <option key={item["category_id"]} value={item["name"]}>
+          {item["name"]}
+        </option>
       ))}
     </select>
   );
