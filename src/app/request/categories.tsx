804bc29f780005c15fd7953060dfa3a5f118164a
Juan Huguet <jhuguetospina@gmail.com>
2024-04-25 23:10:39 -0500

    ADD: setup jwt use in categories query

--- src/app/request/categories.tsx
+++ src/app/request/categories.tsx
@@ -1,24 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useGlobalProp } from "../context/page";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
 
 const Categories = ({ setCategory }: { setCategory: any }) => {
+  const { jwt } = useGlobalProp();
+  const router = useRouter();
   const [categories, setCategories] = useState([]);
   const category_default = "Categoría";
+  
+  const loadCategoryFetch = useCallback(async () => {
+    const token = jwt || localStorage.getItem("sessionToken");
+    if (!token) {
+      alert("You are not authenticated");
+      router.push("/home");
+    } else {
+      try {
+        const { documentId }: { documentId: string } = jwtDecode(token);
+        const response_category = await fetch(
+          "http://localhost:5000/category/",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": token,
+              "documentId": documentId,
+            },
+          }
+        );
 
-  useEffect(() => {
-    const loadCategoryFetch = async () => {
-      const response_category = await fetch("http://localhost:5000/category/", {
-        method: "GET",
-      }).then((response) => {
-        if (response.ok) {
-          return response.json();
+        if (response_category.ok) {
+          const categoriesData = await response_category.json();
+          setCategories(categoriesData);
+        } else {
+          console.error(
+            "Failed to fetch categories:",
+            response_category.statusText
+          );
         }
-        return [];
-      });
-      setCategories(response_category);
-    };
-
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    }
+  }, [jwt]);
+  
+  useEffect(() => {
     loadCategoryFetch();
-  }, []);
+  }, [loadCategoryFetch]);
 
   return (
     <select
@@ -28,12 +56,15 @@ const Categories = ({ setCategory }: { setCategory: any }) => {
         setCategory(value);
       }}
     >
-      <option defaultValue={category_default}>{category_default}</option>
-      {categories.map((item) => (
-        <option key={item["category_id"]} value={item["name"]}>
-          {item["name"]}
-        </option>
-      ))}
+      {categories.length > 0 ? (
+        categories.map((item) => (
+          <option key={item["category_id"]} value={item["name"]}>
+            {item["name"]}
+          </option>
+        ))
+      ) : (
+        <option defaultValue={category_default}>{category_default}</option>
+      )}
     </select>
   );
 };
