df591bbc03db8aafbe47e1c153c309533ce5e77d
Juan Huguet <jhuguetospina@gmail.com>
2024-04-28 17:59:36 -0500

    REFACTOR: PAGE STRUCTURE

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,45 @@
+"use client";
 import { useState } from "react";
 import RequestList from "./requests";
-import Request from "../request/page";
 import People from "./persons";
+import RequestMaker from "../request/page";
+import Entity from "./entity";
 export default function page() {
+  const [showListRequest, setshowListRequest] = useState(false);
+  const [showListPerson, setshowListPerson] = useState(false);
+  const [shownEntity, setShownEntity] = useState({});
+
+  const toggleRequestView = (item: any) => {
+    setShownEntity(item[0]);
+    toggleRequestListView();
+  };
+
+  const togglePersonView = (item: any) => {
+    setShownEntity(item[0]);
+    togglePersonListView();
+  };
+
+  const toggleRequestListView = () => {
+    setshowListRequest(!showListRequest);
+  };
+
+  const togglePersonListView = () => {
+    setshowListPerson(!showListPerson);
+  };
+
   return (
     <>
-      <Request />
-      <People />
-      <RequestList />
+      <RequestMaker />
+      {showListPerson ? (
+        <Entity entity={shownEntity} toggleView={togglePersonListView} isPerson={true}/>
+      ) : (
+        <People viewRequest={togglePersonView} />
+      )}
+      {showListRequest ? (
+        <Entity entity={shownEntity} toggleView={toggleRequestListView} isPerson={false}/>
+      ) : (
+        <RequestList viewRequest={toggleRequestView} />
+      )}
     </>
   );
 }
