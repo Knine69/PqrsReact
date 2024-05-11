2e80e40c5ebc3ecefda558a0ab41bfe24ec4639d
Juan Huguet <jhuguetospina@gmail.com>
2024-05-11 16:07:54 -0500

    REFACTOR: login used variables and toggling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,36 +2,53 @@
 
 import "./login.css";
 import { useRouter } from "next/navigation";
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useGlobalProp } from "../context/page";
 import ContextValidator from "../context/utils";
 
 export default function Page() {
   const router = useRouter();
+
   const [documentId, setDocumentId] = useState("");
   const [password, setPassword] = useState("");
   const [authenticated, setAuthenticated] = useState(false);
+  const [inputClass, setInputClass] = useState("inputValues");
+  const [errorToggle, setErrorToggle] = useState(false);
+
   const { jwt, setJwt } = useGlobalProp();
   const { encryptData } = ContextValidator();
 
   useEffect(() => {
-    if(jwt !== "" && authenticated){
-      router.push("/home")
+    if (jwt !== "" && authenticated) {
+      router.push("/home");
     }
   }, [jwt]);
 
-  const returnRequestBody = (data: FormData ) => {
+  const returnRequestBody = (data: FormData) => {
     const jsonData: any = {};
     data.forEach((value, key) => {
       jsonData[key] = value;
     });
 
-    return jsonData
-};
+    return jsonData;
+  };
+
+  const toggleErrorStyles = (failed: boolean) => {
+    if (failed) {
+      setErrorToggle(true);
+      setInputClass("errorInputValues");
+      return;
+    }
+
+    setErrorToggle(false);
+    setInputClass("inputValues");
+    return;
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     try {
       event.preventDefault();
+      toggleErrorStyles(false);
       const formData = new FormData(event.currentTarget);
       const data = returnRequestBody(formData);
       data.password = encryptData(password);
@@ -43,21 +60,27 @@ export default function Page() {
         body: JSON.stringify(data),
       });
 
-      const { sessionToken, validSession, description }: { sessionToken: string, validSession: boolean, description: string } = await response.json()
+      const {
+        sessionToken,
+        validSession,
+        description,
+      }: { sessionToken: string; validSession: boolean; description: string } =
+        await response.json();
 
-      if(validSession){
-        setJwt(sessionToken)
-        setAuthenticated(validSession)
-        localStorage.setItem("sessionToken", sessionToken)
+      if (validSession) {
+        setJwt(sessionToken);
+        setAuthenticated(validSession);
+        localStorage.setItem("sessionToken", sessionToken);
       } else {
-        alert(`Something went wrong: ${description}`)
+        alert(`Something went wrong: ${description}`);
       }
-    } catch(error) {
-      alert(`Something went wrong: ${error}`)
+    } catch (error) {
+      toggleErrorStyles(true);
+      alert(`Something went wrong: ${error}`);
     }
-    setDocumentId('');
-    setPassword('');
-  }
+    setDocumentId("");
+    setPassword("");
+  };
 
   return (
     <div className="content-container">
@@ -70,8 +93,14 @@ export default function Page() {
             type="text"
             id="documentId"
             name="document"
+            className={inputClass}
             value={documentId}
-            onChange={(event) => setDocumentId(event.target.value)}
+            onChange={(event) => {
+              const filteredValue = event.target.value.replace(/[^0-9]/g, '');
+              if(filteredValue) {
+                setDocumentId(filteredValue);
+              }
+            }}
           />
         </div>
 
@@ -81,13 +110,18 @@ export default function Page() {
             type="password"
             id="password"
             name="password"
+            className={inputClass}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
         </div>
-
+        {errorToggle && (
+          <div>
+            <p className="errorText">Por favor intenta ingresar nuevamente.</p>
+          </div>
+        )}
         <button type="submit" className="select-button">
-          Submit
+          Login
         </button>
       </form>
     </div>
